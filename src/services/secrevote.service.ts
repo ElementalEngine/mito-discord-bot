6befc06736a62c9f9a6003b74997601070ff239c
ProjectCisco <sga.accounts@proton.me>
2026-03-13 14:39:58 +0000

    Secretvote file clean up

--- src/services/secrevote.service.ts
+++
@@ -1,8 +0,0 @@
-export {
-  evaluateSecretVoteOutcome,
-  isSecretVoteActive,
-  recordSecretVoteChoice,
-  startSecretVote,
-} from './secretvote/secretvote.service.js';
-
-export type { RecordSecretVoteResult } from './secretvote/secretvote.service.js';
